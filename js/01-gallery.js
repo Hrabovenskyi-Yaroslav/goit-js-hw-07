40ee4bd3f720318e78bad6698f839c3c138003ae
Hrabovenskyi-Yaroslav <grabovenskyi@gmail.com>
2022-02-04 19:49:38 +0200

    end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const galleryRef = document.querySelector('.gallery');
+const cardsMakrup = createCardsMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', cardsMakrup);
+galleryRef.addEventListener('click', onCardsClick);
+
+function createCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`
+    })
+        .join('');
+}
+
+function onCardsClick(event) {
+    const isImageEl = event.target.classList.contains('gallery__image');
+    const imageSrc = event.target.dataset.source;
+    
+    event.preventDefault();
+    if (!isImageEl) {
+        return;
+    }
+    
+    const imageShow = basicLightbox.create(`
+    <img src="${imageSrc}">
+    `, {onClose: () => {document.removeEventListener("keydown", closeByKeyboard);
+    }
+    })
+    
+    imageShow.show()
+
+    document.addEventListener("keydown", closeByKeyboard);
+ 
+    function closeByKeyboard(event) {
+        if (event.key != 'Escape') {
+            return;
+        }
+        imageShow.close()
+    }
+    
+};
